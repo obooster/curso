8b9b841ab6afd4d413f13cd65f7433ddd23caa6b
obooster <wellingtonkr.pessoal@gmail.com>
2025-06-05 11:16:20 -0300

    Commit

---
+++ src/main/java/me/wellington/curso/database/Main.java
@@ -0,0 +1,13 @@
+package me.wellington.curso.database;
+
+public final class Main {
+
+    public void init() {
+
+    }
+
+    public static void main(String[] args) {
+        new Main().init();
+    }
+
+}
