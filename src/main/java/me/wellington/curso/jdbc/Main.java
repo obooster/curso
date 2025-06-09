127086a32ab0d72528297ad7429567ec96a4f491
obooster <wellingtonkr.pessoal@gmail.com>
2025-06-09 10:14:02 -0300

    Commit

--- src/main/java/me/wellington/curso/jdbc/Main.java
+++ src/main/java/me/wellington/curso/jdbc/Main.java
@@ -2,24 +2,34 @@ package me.wellington.curso.jdbc;
 
 import me.wellington.curso.jdbc.objects.Profile;
 import me.wellington.curso.jdbc.services.ProfileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Scanner;
+import java.util.logging.Level;
 
 public final class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     private final ProfileService profileService = new ProfileService();
+
+    static {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "warn");
+        System.setProperty("org.slf4j.simpleLogger.log.com.zaxxer.hikari", "off");
+    }
 
     public void init() {
         profileService.setupProfiles();
 
         var scanner = new Scanner(System.in);
-        System.out.println("Bom dia! Escreva seu nome a seguir: ");
+        System.out.print("Bom dia! Escreva seu nome a seguir: ");
         var name = scanner.nextLine();
-        System.out.println("Certo! Agora a sua idade: ");
+        System.out.print("Certo! Agora a sua idade: ");
         var age = scanner.nextInt();
 
         profileService.register(new Profile(name, age));
-        System.out.println("Registrado com sucesso! Segue pessoas registradas a seguir:\n\n");
-        profileService.getRepository().forEach(System.out::println);
+        System.out.println("Registrado com sucesso! Segue pessoas registradas a seguir:\n");
+
+        profileService.getRepository().stream().map(profile -> profile.getName() +  " | " + profile.getAge() + " anos").forEach(System.out::println);
     }
 
     public static void main(String[] args) {
